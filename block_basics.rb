d972f2d98ce4bc5e3c44506bcd19293a05cd0a24
Alex <alexvdberg010109@gmail.com>
2024-09-12 10:29:46 +0200

    Enumerable part 1

--- block_basics.rb
+++ block_basics.rb
@@ -1,5 +1,24 @@
-5.times do
-  puts "situps"
-  puts "pushups"
-  puts "chinups"
+class Flyer
+  attr_reader :name, :email, :miles_flown
+  attr_accessor :status
+
+  def initialize(name, email, miles_flown, status=:bronze)
+    @name = name
+    @email = email
+    @miles_flown = miles_flown
+    @status = status
+  end
+
+  def to_s
+    "#{name} (#{email}): #{miles_flown} - #{status}"
+  end
 end
+
+flyers = []
+flyers << Flyer.new("Larry", "larry@example.com", 4000, :platinum)
+flyers << Flyer.new("Moe", "moe@example.com", 1000)
+flyers << Flyer.new("Curly", "curly@example.com", 3000, :gold)
+flyers << Flyer.new("Shemp", "shemp@example.com", 2000)
+
+
+puts flyers.find { |flyer| flyer.status == :bronze }
